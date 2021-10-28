fb02fc75670a3d90a3501e5a111224ff253e0252
James-Bolt <gleb.frolow2013@gmail.com>
2021-10-28 20:31:50 +0300

    Start

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import cv2
+import numpy
+import Samples
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    img = cv2.imread("Samples/1.jpg")
+    (b, g, r) = img[0, 0]
+    cv2.imshow("Display window", img)
+    k = cv2.waitKey(0)
